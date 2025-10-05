2fc874f7d22ddbc3348b93f02e224a17b58374b6
Iliya Kruglov <iliya.kruglov@student.heaj.be>
2025-10-05 12:27:02 +0200

    Made enum folder not have errors

--- Assets/Projects/Editor/SimpleAudioEditor.cs
+++ Assets/Projects/Editor/SimpleAudioEditor.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-//using System.Collections.Generic;
 
 public class SimpleAudioEditor : EditorWindow
 {
@@ -25,8 +24,8 @@ public class SimpleAudioEditor : EditorWindow
         sfxName = EditorGUILayout.TextField("SFX Group Name", sfxName);
         if (GUILayout.Button("Add SFX Group"))
         {
-            Debug.Log(sfxName);
-            //CreateFolder(sfxPath, sfxName);
+            //Debug.Log(sfxName);
+            CreateFolder(sfxPath, sfxName);
         }
 
         if (GUILayout.Button("Refresh Sound List"))
@@ -36,47 +35,88 @@ public class SimpleAudioEditor : EditorWindow
     }
     private void CreateFolder(string soundType, string folderName)
     {
-        AssetDatabase.CreateFolder(pluginPath + soundType, folderName);
-        string guid = AssetDatabase.CreateFolder(pluginPath + soundType + folderName, $"New {soundType}");
-        string newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
-        Debug.Log(newFolderPath);
+        if(folderName.Contains(" "))
+        {
+            Debug.LogWarning("Cannot create a sound with space, please remove the space or change the name.");
+            return;
+        }
+        if (!Directory.Exists(pluginPath + soundType + "/" + folderName))
+        {
+            AssetDatabase.CreateFolder(pluginPath + soundType, folderName);
+        }
+        else
+        {
+            Debug.LogWarning("Please write a nonexistant sound, or increment it without spaces.");
+        }
+        //string guid = AssetDatabase.CreateFolder(pluginPath + soundType + folderName, $"New {soundType}");
+        //string newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+        //Debug.Log(newFolderPath);
     }
 
     private void RefreshEnum()
     {
-        if (!File.Exists(pluginPath + "/SoundList.cs"))
+        string enumDir = String.Join("," + Environment.NewLine + "    ", GetAllFolders());
+
+        Debug.Log(enumDir);
+
+        if (File.Exists(pluginPath + "/SoundList.cs"))
         {
+            File.Delete(pluginPath + "/SoundList.cs");
             using (FileStream fs = File.Create(pluginPath + "/SoundList.cs"))
             {
                 AddText(fs, "public enum Sound" + Environment.NewLine +
                             "{" + Environment.NewLine +
-                            "   " + sfxPath + Environment.NewLine +
+                            "    " + enumDir + Environment.NewLine +
                             "}");
             }
         }
 
-        RemoveLastLine(pluginPath + "/SoundList.cs");
+        //GetAllFolders();
+
+        /*string pastFileString = String.Join(Environment.NewLine, RemoveLastLine(pluginPath + "/SoundList.cs"));
 
         using (FileStream fs = File.OpenWrite(pluginPath + "/SoundList.cs"))
         {
-            AddText(fs, "," +
+            AddText(fs,
+                        pastFileString +
+                        "," +
                        $"\r\n   {musicPath}" +
                         "\r\n}");
-        }
+        }*/
         AssetDatabase.Refresh();
 
     }
 
-    private void AddText(FileStream fs, string value, int? lineLength = null)
+    private void AddText(FileStream fs, string value)
     {
         byte[] info = new UTF8Encoding(true).GetBytes(value);
-        fs.Write(info, (lineLength.HasValue ? lineLength: 0), info.Length);
+        fs.Write(info, 0, info.Length);
     }
 
-    private void RemoveLastLine(string enumPath)
+    private List<string> RemoveLastLine(string enumPath)
     {
         List<string> lines = File.ReadLines(enumPath).ToList();
         lines.RemoveAt(lines.Count - 1);
-        File.WriteAllLines(enumPath, lines);
+        return lines;
+        /*File.WriteAllLines(enumPath, lines);
+        return lines.Count - 1;*/
+    }
+
+    private string[] GetAllFolders()
+    {
+        List<string> dir = new List<string>(Directory.EnumerateDirectories(pluginPath + sfxPath));
+        string[] allFolders = new string[dir.Count];
+
+        for(int i = 0; i < dir.Count; i++)
+        {
+            allFolders[i] = dir[i].Remove(0, (pluginPath + sfxPath).Length + 1);
+        }
+
+        return allFolders;
+
+        /*foreach(string folder in allFolders)
+        {
+            Debug.Log(folder);
+        }*/
     }
 }
